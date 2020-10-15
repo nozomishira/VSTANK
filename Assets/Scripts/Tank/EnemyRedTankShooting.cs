64a3a88c8f70a988be7ef1f3471d8019d68ad667
nozomishira <baishiwang1213@gmail.com>
2020-10-15 19:14:37 +0900

    Added Shell for black Enemy

--- Assets/Scripts/Tank/EnemyRedTankShooting.cs
+++ Assets/Scripts/Tank/EnemyRedTankShooting.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class EnemyRedTankShooting : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float timercount = 0.0f;
+    public Rigidbody enemy_Shell;
+    public Transform enemy_FireTransform;
+    public float mLaunchForce = 15f;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timercount += Time.deltaTime;
+        if (timercount > 4.0f)
+        {
+            fire();
+        }
+    }
+
+    private void fire()
+    {
+
+        Rigidbody shellInstance = Instantiate(enemy_Shell, enemy_FireTransform.position, enemy_FireTransform.rotation) as Rigidbody;
+        shellInstance.velocity = mLaunchForce * enemy_FireTransform.forward * 2;
+        timercount = 0;
     }
 }
