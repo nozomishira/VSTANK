07d22ab243de7ce791f78597fada60aa268004c1
yama2746 <yamada329329@gmail.com>
2020-11-04 18:38:48 +0900

    StartSceneを編集

--- Assets/Scripts/Tank/StartGame.cs
+++ Assets/Scripts/Tank/StartGame.cs
@@ -19,6 +19,6 @@ public class StartGame : MonoBehaviour
 
     public void onClickStart()
     {
-        SceneManager.LoadScene("ExplainScene");
+        SceneManager.LoadScene("LevelChooseScene");
     }
 }
